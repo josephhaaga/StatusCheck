ab6c9ff84723fa3d288021c3aec55ec99a9fd79b
Joseph Haaga <joseph@Joseph-Dev-MBP-R-275.local>
2017-01-30 13:12:27 -0500

    remove delay

--- statuscheck/sandbox.py
+++ statuscheck/sandbox.py
@@ -1,11 +1,11 @@
 import functions;
 import os;
-from time import sleep
+from time import sleep;
 
 list_of_sites = open(os.path.dirname(os.path.abspath(__file__))+'/settings.conf');
 
 for n in list_of_sites:
-	sleep(1);
+	# sleep(1);
 	m = n.replace("\n","");
 	functions.check_google_analytics(m);
 
